3e40dd25dbc76b3883943d7397fb51c68e3652cd
Quercaesii <somsit.thanapat@gmail.com>
2025-01-21 17:27:57 +0700

    update: change Board class structure: using static classes instead

--- src/main/scala/com/chess8007/app/services/logics/Board.scala
+++ src/main/scala/com/chess8007/app/services/logics/Board.scala
@@ -7,6 +7,7 @@ import com.shogi8017.app.errors.*
 import GameEvent.{CHECK, CHECKMATE, DEAD_POSITION, STALEMATE}
 import com.shogi8017.app.services.*
 import com.shogi8017.app.services.logics.PieceType.{BISHOP, KING, KNIGHT, PAWN, QUEEN, ROOK}
+import com.shogi8017.app.services.logics.Player.{BLACK_PLAYER, WHITE_PLAYER}
 
 type MoveResult = (Board, StateTransitionList, AlgebraicNotation, Option[GameEvent])
 type BoardStateTransition = (Board, StateTransitionList, AlgebraicNotation)
@@ -18,18 +19,57 @@ type AlgebraicNotation = String
 enum BoardAction: 
   case REMOVE, ADD
 
-case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
-  def executeMove(player: Player, playerAction: PlayerAction): Validated[GameValidationError, MoveResult] = {
-    
+case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {}
+
+object Board {
+  def defaultInitialPosition: Board = {
+    def createPawns(player: Player, row: Int): Map[Position, Piece] =
+      (1 to 8).map(col => Position(col, row) -> Pawn(player)).toMap
+
+    def createMajorPieces(player: Player, row: Int): Map[Position, Piece] = {
+      val pieceOrder = Seq(
+        Rook(player), Knight(player), Bishop(player),
+        Queen(player), King(player),
+        Bishop(player), Knight(player), Rook(player)
+      )
+      pieceOrder.zipWithIndex.map { case (piece, col) =>
+        Position(col + 1, row) -> piece
+      }.toMap
+    }
+
+    val whitePieces = createMajorPieces(Player.WHITE_PLAYER, 1) ++ createPawns(Player.WHITE_PLAYER, 2)
+    val blackPieces = createMajorPieces(Player.BLACK_PLAYER, 8) ++ createPawns(Player.BLACK_PLAYER, 7)
+
+    Board(whitePieces ++ blackPieces)
+  }
+
+  def emptyBoard: Board = Board(
+    Map(Position(5,1) -> King(WHITE_PLAYER), Position(5,8) -> King(BLACK_PLAYER)),
+    None
+  )
+
+  def fromMovesList(moveList: List[(Player, PlayerAction)]): Validated[GameValidationError, Board] = {
+    moveList.foldLeft(Valid(Board.defaultInitialPosition): Validated[GameValidationError, Board]) { (validatedBoard, move) =>
+      validatedBoard.andThen { board =>
+        val (player, playerAction) = move
+        executeMove(board, player, playerAction).map {
+          case (newBoard, _, _, _) => newBoard
+        }
+      }
+    }
+  }
+
+  def executeMove(board: Board, player: Player, playerAction: PlayerAction): Validated[GameValidationError, MoveResult] = {
+
     def validateGameState(player: Player): Validated[GameValidationError, Unit] = {
-      Validated.cond(getKingPosition(player).nonEmpty, (), NoKingError)
+      Validated.cond(getKingPosition(board, player).nonEmpty, (), NoKingError)
     }
 
     def validateMove(player: Player, playerAction: PlayerAction): Validated[MoveValidationError, Unit] = {
       val (from, to) = playerAction.getFromToPositions
-      
+
       val errors = List(
-        if (isOutOfTurn(lastMove, player)) Some(OutOfTurn) else None,
+        if (isOutOfTurn(board.lastMove, player)) Some(OutOfTurn) else None,
         if (from == to) Some(NoMove) else None,
         if (to.isOutOfBoard) Some(OutOfBoard) else None
       ).flatten
@@ -41,12 +81,12 @@ case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
     }
 
     def validatePieceExistence(from: Position): Validated[MoveValidationError, Piece] = {
-      Validated.cond(pieces.contains(from), pieces(from), UnoccupiedPosition)
+      Validated.cond(board.pieces.contains(from), board.pieces(from), UnoccupiedPosition)
     }
 
-    def processMove(playerAction: PlayerAction)(piece: Piece): (Piece, Validated[GameValidationError, BoardStateTransition]) = {
-      (piece, piece.getBoardTransition(this, playerAction).andThen((stateTransitionList, algebraicNotation) => {
-        val newPiecesMap = stateTransitionList.foldLeft(pieces) { (acc, transition) =>
+    def processMove(board: Board, playerAction: PlayerAction)(piece: Piece): (Piece, Validated[GameValidationError, BoardStateTransition]) = {
+      (piece, piece.getBoardTransition(board, playerAction).andThen((stateTransitionList, algebraicNotation) => {
+        val newPiecesMap = stateTransitionList.foldLeft(board.pieces) { (acc, transition) =>
           val (action, position, player, pieceType) = transition
           action match {
             case BoardAction.REMOVE => acc - position
@@ -64,7 +104,7 @@ case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
         }
 
         val (from, to, promoteTo) = playerAction.getFields
-        val newBoard = copy(
+        val newBoard = board.copy(
           pieces = newPiecesMap,
           lastMove = Some(Move(player, from, to, piece, promoteTo))
         )
@@ -79,8 +119,8 @@ case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
         val gameEvent = checkGameEvent(board, movingPiece.owner)
 
         val newAlgebraicNotation = gameEvent match
-          case Some(CHECK) | Some(CHECKMATE)          => algebraicNotation + (if gameEvent.contains(CHECK) then " +" else " #")
-          case Some(STALEMATE) | Some(DEAD_POSITION)  => algebraicNotation + " 1/2-1/2"
+          case Some(CHECK) | Some(CHECKMATE) => algebraicNotation + (if gameEvent.contains(CHECK) then " +" else " #")
+          case Some(STALEMATE) | Some(DEAD_POSITION) => algebraicNotation + " 1/2-1/2"
           case None => algebraicNotation
 
         Valid((board, stateTransitionList, algebraicNotation, gameEvent))
@@ -88,10 +128,13 @@ case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
     }
 
     def checkGameEvent(board: Board, player: Player): Option[GameEvent] = {
-      if (isChecked(player)) Some(CHECK)
-      else if (isCheckmated(board, player)) Some(CHECKMATE)
-      else if (isStalemate(board, player)) Some(STALEMATE)
-      else if (board.isDeadPosition(board)) Some(DEAD_POSITION)
+      val opponent = if player == WHITE_PLAYER then BLACK_PLAYER else WHITE_PLAYER
+      if (isChecked(board, opponent)) {
+        if (isCheckmated(board, opponent)) Some(CHECKMATE)
+        else Some(CHECK)
+      }
+      else if (isStalemate(board, opponent)) Some(STALEMATE)
+      else if (isDeadPosition(board)) Some(DEAD_POSITION)
       else None
     }
 
@@ -99,15 +142,17 @@ case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
     validateGameState(player)
       .andThen(_ => validateMove(player, playerAction))
       .andThen(_ => validatePieceExistence(from))
-      .andThen(processGameEvent compose processMove(playerAction))
+      .andThen(processGameEvent compose processMove(board, playerAction))
   }
 
   private def isOutOfTurn(lastMove: Option[Move], currentPlayer: Player): Boolean =
-    lastMove.exists(_.player == currentPlayer)
+    lastMove match
+      case None => currentPlayer == BLACK_PLAYER
+      case Some(lastMove) => lastMove.player == currentPlayer
 
   private def isStalemate(board: Board, player: Player): Boolean = {
-    !isChecked(player) && !forallPlayerPieces(player) { (position, piece) =>
-      piece.hasLegalMoves(this, position)
+    !isChecked(board, player) && !forallPlayerPieces(board, player) { (position, piece) =>
+      piece.hasLegalMoves(board, position)
     }
   }
 
@@ -119,16 +164,16 @@ case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
 
     // Case 2: King and bishop vs. King or King and knight vs. King
     def isKingAndBishopVsKingOrKingAndKnightVsKing: Boolean = {
-      pieces.size == 2 && pieces.exists(_.isInstanceOf[King]) &&
-        pieces.exists(p => p.isInstanceOf[Bishop] || p.isInstanceOf[Knight])
+      pieces.size == 3 && pieces.count(_.isInstanceOf[King]) == 2 &&
+        pieces.exists(p => p.isInstanceOf[Knight] || p.isInstanceOf[Bishop])
     }
 
     // Case 3: King and bishop vs. King and bishop of opposite colors
     def isKingAndBishopVsKingAndBishop: Boolean = {
       pieces.size == 4 && {
         val bishops = board.pieces.collect { case (position: Position, b: Bishop) => (position, b) }
-        if(bishops.size != 2) return false
-        
+        if (bishops.size != 2) return false
+
         bishops.headOption.flatMap { case (pos1, b1) =>
           bishops.tail.headOption.map { case (pos2, b2) =>
             b1.owner != b2.owner && pos1.getPositionColor == pos2.getPositionColor
@@ -137,56 +182,56 @@ case class Board(pieces: Map[Position, Piece], lastMove: Option[Move] = None) {
       }
     }
 
+    val (x, y, z) = (isKingVsKing, isKingAndBishopVsKingOrKingAndKnightVsKing, isKingAndBishopVsKingAndBishop)
+
     isKingVsKing || isKingAndBishopVsKingOrKingAndKnightVsKing || isKingAndBishopVsKingAndBishop
   }
 
-
-  def isChecked(player: Player): Boolean = {
-    val kingPosition = getKingPosition(player)
-    kingPosition.exists(_.isUnderAttack(this, player))
+  def isChecked(board: Board, player: Player): Boolean = {
+    val kingPosition = getKingPosition(board, player)
+    val k = kingPosition.exists(_.isUnderAttack(board, player))
+    kingPosition.exists(_.isUnderAttack(board, player))
   }
 
   private def isCheckmated(board: Board, player: Player): Boolean = {
     def hasEscape(player: Player): Boolean = {
-      existsPlayerPieces(player) { (position, piece) =>
-        piece.getAllPossibleMoves(this, position).exists { to =>
-          val tempBoard = copy(
-            pieces = pieces - position + (to -> piece),
+      existsPlayerPieces(board, player) { (position, piece) =>
+        piece.getAllPossibleMoves(board, position).exists { to =>
+          val tempBoard = board.copy(
+            pieces = board.pieces - position + (to -> piece),
             lastMove = Some(Move(player, position, to, piece))
           )
 
           // The invalidity of the `isCheck` method can be disregarded here, as it will only be invalid if there is no king.
           // If there is no king, this function will not be invoked in the first place.
-          tempBoard.isChecked(player)
+          val k = !isChecked(tempBoard, player)
+          !isChecked(tempBoard, player)
         }
       }
     }
 
-    isChecked(player) && !hasEscape(player)
+    isChecked(board, player) && !hasEscape(player)
   }
 
-  // TODO: check other cases of draw
-  
-  def existsPlayerPieces(player: Player)(f: (Position, Piece) => Boolean): Boolean = {
-    pieces.filter {
+  def existsPlayerPieces(board: Board, player: Player)(f: (Position, Piece) => Boolean): Boolean = {
+    board.pieces.filter {
       case (position, piece) => piece.owner == player
     }.exists {
       case (position, piece) => f(position, piece)
     }
   }
 
-  private def forallPlayerPieces(player: Player)(f: (Position, Piece) => Boolean): Boolean = {
-    pieces.filter {
+  private def forallPlayerPieces(board: Board, player: Player)(f: (Position, Piece) => Boolean): Boolean = {
+    board.pieces.filter {
       case (position, piece) => piece.owner == player
     }.forall {
       case (position, piece) => f(position, piece)
     }
   }
 
-  private def getKingPosition(player: Player): Option[Position] = {
-    pieces.collectFirst {
+  private def getKingPosition(board: Board, player: Player): Option[Position] = {
+    board.pieces.collectFirst {
       case (position, king: King) if king.owner == player => Some(position)
     }.flatten
   }
-
 }
