b493bb86bb29e68866743ffa91ec0f82af76c250
Quercaesii <somsit.thanapat@gmail.com>
2025-02-03 22:40:54 +0700

    Add unit tests for bishops

--- src/test/scala/com/shogi8017/app/services/logics/BishopTest.scala
+++ src/test/scala/com/shogi8017/app/services/logics/BishopTest.scala
@@ -1,83 +1,157 @@
 package com.shogi8017.app.services.logics
 
-import com.shogi8017.app.errors.IllegalMove
+import com.shogi8017.app.errors.{IllegalMove, IncorrectPromotionScenario}
 import com.shogi8017.app.services.logics.LogicTestUtils.*
 import com.shogi8017.app.services.logics.Player.{BLACK_PLAYER, WHITE_PLAYER}
+import com.shogi8017.app.services.logics.pieces.{Knight, PromotedBishop, Bishop}
 import org.scalatest.funsuite.AnyFunSuite
 
 class BishopTest extends AnyFunSuite:
-  test("A bishop should move diagonally") {
-    val emptyBoard = Board.emptyBoard
-    val newPieces = emptyBoard.pieces
-      + (Position(4, 4) -> Bishop(WHITE_PLAYER, true))
-      + (Position(5,4) -> Bishop(BLACK_PLAYER, true))
-
-    val newBoard1 = Board(newPieces)
-    val whitePos = Position(4,4)
-    val testingMoves1 = List((-3,-3),(4,4),(3,-3),(-3,3))
-    testingMoves1.foreach((x, y) => testMove(WHITE_PLAYER, PlayerAction(whitePos, whitePos.move(x, y)), Bishop(WHITE_PLAYER, true), newBoard1))
-
-    val newBoard2 = Board(newPieces, Some(Move(WHITE_PLAYER, Position(1, 1), Position(4, 4), Bishop(WHITE_PLAYER, true), None)))
-    val blackPos = Position(5,4)
-    val testingMoves2 = List((-3,-3),(3,3),(3,-3),(-3,3))
-    testingMoves2.foreach((x, y) => testMove(BLACK_PLAYER, PlayerAction(blackPos, blackPos.move(x, y)), Bishop(BLACK_PLAYER, true), newBoard2))
+  private def generateDiagonalPositions(start: Position): Seq[Position] = {
+    val directions = Seq((-1, -1), (1, 1), (-1, 1), (1, -1))
+    val range = 1 to 9
+    directions
+      .flatMap { case (dx, dy) => range.map(i => Position(start.x + i * dx, start.y + i * dy)) }
+      .filter(pos => pos.x >= 1 && pos.x <= 9 && pos.y >= 1 && pos.y <= 9 && pos != start)
   }
 
-  test("A bishop should not move like a cross") {
-    val emptyBoard = Board.emptyBoard
-    val newPieces = emptyBoard.pieces
-      + (Position(4, 4) -> Bishop(WHITE_PLAYER, true))
-      + (Position(5, 4) -> Bishop(BLACK_PLAYER, true))
+  test("A Bishop should move diagonally") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(4, 4) -> Bishop(WHITE_PLAYER),
+        Position(4, 6) -> Bishop(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
 
-    val newBoard1 = Board(newPieces)
-    testMoveError(WHITE_PLAYER, PlayerAction(Position(4, 4), Position(4, 8)), IllegalMove, newBoard1)
-    testMoveError(WHITE_PLAYER, PlayerAction(Position(4, 4), Position(4, 3)), IllegalMove, newBoard1)
-    testMoveError(WHITE_PLAYER, PlayerAction(Position(4, 4), Position(8, 4)), IllegalMove, newBoard1)
-    testMoveError(WHITE_PLAYER, PlayerAction(Position(4, 4), Position(2, 4)), IllegalMove, newBoard1)
+    val testSeqWhite = generateDiagonalPositions(Position(4, 4))
+    testSeqWhite.foreach(pos => testMove(WHITE_PLAYER, MoveAction(Position(4, 4), pos), Bishop(WHITE_PLAYER), s0))
 
-    val newBoard2 = Board(newPieces, Some(Move(WHITE_PLAYER, Position(1, 1), Position(4, 4), Bishop(WHITE_PLAYER, true), None)))
-    testMoveError(BLACK_PLAYER, PlayerAction(Position(5, 4), Position(7, 4)), IllegalMove, newBoard2)
-    testMoveError(BLACK_PLAYER, PlayerAction(Position(5, 4), Position(3, 4)), IllegalMove, newBoard2)
-    testMoveError(BLACK_PLAYER, PlayerAction(Position(5, 4), Position(5, 6)), IllegalMove, newBoard2)
-    testMoveError(BLACK_PLAYER, PlayerAction(Position(5, 4), Position(5, 2)), IllegalMove, newBoard2)
+    val testSeqBlack = generateDiagonalPositions(Position(4, 6))
+    testSeqBlack.foreach(pos => testMove(BLACK_PLAYER, MoveAction(Position(4, 6), pos), Bishop(BLACK_PLAYER), s1))
   }
 
-  test("A Bishop should capture diagonally") {
-    val emptyBoard = Board.emptyBoard
-    val newPieces = emptyBoard.pieces
-      + (Position(4, 4) -> Bishop(WHITE_PLAYER, true))
-      + (Position(6, 2) -> Bishop(BLACK_PLAYER, true))
+  test("A Bishop should not move like something else") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(4, 4) -> Bishop(WHITE_PLAYER),
+        Position(4, 6) -> Bishop(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
 
-    val newBoard1 = Board(newPieces)
-    testMove(WHITE_PLAYER, PlayerAction(Position(4, 4), Position(6, 2)), Bishop(WHITE_PLAYER, true), newBoard1)
+    val allPositions = for {
+      row <- 1 to 9
+      col <- 1 to 9
+    } yield Position(row, col)
 
-    val newBoard2 = Board(newPieces, Some(Move(WHITE_PLAYER, Position(1, 1), Position(4, 4), Bishop(WHITE_PLAYER, true), None)))
-    testMove(BLACK_PLAYER, PlayerAction(Position(6, 2), Position(4, 4)), Bishop(BLACK_PLAYER, true), newBoard2)
+    val whiteReachablePositions = generateDiagonalPositions(Position(4, 4))
+    val testSeqWhite = allPositions.filterNot(whiteReachablePositions.contains)
+    testMoveError(WHITE_PLAYER, MoveAction(Position(4, 4), testSeqWhite.head), IllegalMove, s0)
+
+    val blackReachablePositions = generateDiagonalPositions(Position(4, 6))
+    val testSeqBlack = allPositions.filterNot(blackReachablePositions.contains)
+    testMoveError(BLACK_PLAYER, MoveAction(Position(4, 6), testSeqBlack.head), IllegalMove, s1)
   }
 
-  test("A bishop should not capture like a cross") {
-    val emptyBoard = Board.emptyBoard
-    val newPieces = emptyBoard.pieces
-      + (Position(4, 4) -> Bishop(WHITE_PLAYER, true))
-      + (Position(6, 4) -> Bishop(BLACK_PLAYER, true))
-    
-    val newBoard1 = Board(newPieces)
-    testMoveError(WHITE_PLAYER, PlayerAction(Position(4, 4), Position(6, 4)), IllegalMove, newBoard1)
-    
-    val newBoard2 = Board(newPieces, Some(Move(WHITE_PLAYER, Position(1, 1), Position(4, 4), Bishop(WHITE_PLAYER, true), None)))
-    testMoveError(BLACK_PLAYER, PlayerAction(Position(6, 4), Position(4, 4)), IllegalMove, newBoard2)
+  test("A Bishop should be able to jump") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(4, 4) -> Bishop(WHITE_PLAYER),
+        Position(7, 7) -> Bishop(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
+
+    testMoveError(WHITE_PLAYER, MoveAction(Position(4, 4), Position(8, 8)), IllegalMove, s0)
+    testMoveError(BLACK_PLAYER, MoveAction(Position(7, 7), Position(2, 2)), IllegalMove, s1)
   }
 
-  test("A Bishop should not jump pass another piece") {
-    val emptyBoard = Board.emptyBoard
-    val newPieces = emptyBoard.pieces
-      + (Position(4, 4) -> Bishop(WHITE_PLAYER, true))
-      + (Position(8, 8) -> Bishop(BLACK_PLAYER, true))
-      + (Position(6, 6) -> Rook(WHITE_PLAYER, true))
-    
-    val newBoard1 = Board(newPieces)
-    testMoveError(WHITE_PLAYER, PlayerAction(Position(4, 4), Position(7, 7)), IllegalMove, newBoard1)
-    
-    val newBoard2 = Board(newPieces, Some(Move(WHITE_PLAYER, Position(1, 1), Position(4, 4), Bishop(WHITE_PLAYER, true), None)))
-    testMoveError(BLACK_PLAYER, PlayerAction(Position(6, 6), Position(5, 5)), IllegalMove, newBoard2)
+  test("A Bishop should capture like a cross") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(4, 4) -> Bishop(WHITE_PLAYER),
+        Position(7, 7) -> Bishop(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
+
+    val r1 = testMove(WHITE_PLAYER, MoveAction(Position(4, 4), Position(7, 7)), Bishop(WHITE_PLAYER), s0)
+    val r2 = testMove(BLACK_PLAYER, MoveAction(Position(7, 7), Position(4, 4)), Bishop(BLACK_PLAYER), s1)
+    assert(r1.piecesMap.size == 3)
+    assert(r2.piecesMap.size == 3)
   }
+
+  test("A Bishop should not capture a piece of its own side") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(4, 4) -> Bishop(WHITE_PLAYER),
+        Position(6, 7) -> Knight(WHITE_PLAYER),
+        Position(4, 6) -> Bishop(BLACK_PLAYER),
+        Position(2, 6) -> Knight(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
+
+    testMoveError(WHITE_PLAYER, MoveAction(Position(6, 6), Position(6, 7)), IllegalMove, s0)
+    testMoveError(BLACK_PLAYER, MoveAction(Position(4, 6), Position(2, 6)), IllegalMove, s1)
+  }
+
+  test("A Bishop should not capture like something else") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(4, 4) -> Bishop(WHITE_PLAYER),
+        Position(4, 6) -> Bishop(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
+
+    testMoveError(WHITE_PLAYER, MoveAction(Position(4, 4), Position(4, 6)), IllegalMove, s0)
+    testMoveError(BLACK_PLAYER, MoveAction(Position(4, 6), Position(4, 4)), IllegalMove, s1)
+  }
+
+  test("Lace should be able to promote when reaching or leaving the last three ranks") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(1, 6) -> Bishop(WHITE_PLAYER),
+        Position(1, 9) -> Bishop(WHITE_PLAYER),
+        Position(1, 4) -> Bishop(BLACK_PLAYER),
+        Position(1, 1) -> Bishop(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
+
+    val testSeqWhite1 = generateDiagonalPositions(Position(1, 6)).filter(_.y >= 7)
+    testSeqWhite1.foreach(pos => testMove(WHITE_PLAYER, MoveAction(Position(1, 6), pos, false), Bishop(WHITE_PLAYER), s0))
+    testSeqWhite1.foreach(pos => testMove(WHITE_PLAYER, MoveAction(Position(1, 6), pos, true),  PromotedBishop(WHITE_PLAYER), s0))
+
+    val testSeqWhite2 = generateDiagonalPositions(Position(1, 9))
+    testSeqWhite2.foreach(pos => testMove(WHITE_PLAYER, MoveAction(Position(1, 9), pos, false), Bishop(WHITE_PLAYER), s0))
+    testSeqWhite2.foreach(pos => testMove(WHITE_PLAYER, MoveAction(Position(1, 9), pos, true),  PromotedBishop(WHITE_PLAYER), s0))
+
+    val testSeqBlack1 = generateDiagonalPositions(Position(1, 4)).filter(_.y <= 3)
+    testSeqBlack1.foreach(pos => testMove(BLACK_PLAYER, MoveAction(Position(1, 4), pos, false), Bishop(BLACK_PLAYER), s1))
+    testSeqBlack1.foreach(pos => testMove(BLACK_PLAYER, MoveAction(Position(1, 4), pos, true),  PromotedBishop(BLACK_PLAYER), s1))
+
+    val testSeqBlack2 = generateDiagonalPositions(Position(1, 1))
+    testSeqBlack2.foreach(pos => testMove(BLACK_PLAYER, MoveAction(Position(1, 1), pos, false), Bishop(BLACK_PLAYER), s1))
+    testSeqBlack2.foreach(pos => testMove(BLACK_PLAYER, MoveAction(Position(1, 1), pos, true),  PromotedBishop(BLACK_PLAYER), s1))
+  }
+
+  test("Bishop cannot promote outside the last three rank") {
+    val s0 = Board.emptyBoard.copy(
+      piecesMap = Board.emptyBoard.piecesMap ++ Map(
+        Position(4, 4) -> Bishop(WHITE_PLAYER),
+        Position(4, 6) -> Bishop(BLACK_PLAYER)
+      )
+    )
+    val s1 = s0.copy(lastAction = Some(Action(WHITE_PLAYER)))
+
+    val testSeqWhite = generateDiagonalPositions(Position(4, 4)).filter(_.y <= 6)
+    testSeqWhite.foreach(pos => testMoveError(WHITE_PLAYER, MoveAction(Position(4, 4), pos, true), IncorrectPromotionScenario, s0))
+
+    val testSeqBlack = generateDiagonalPositions(Position(4, 6)).filter(_.y >= 4)
+    testSeqBlack.foreach(pos => testMoveError(BLACK_PLAYER, MoveAction(Position(4, 6), pos, true), IncorrectPromotionScenario, s1))
+  }
+
+// TODO: drop tests
